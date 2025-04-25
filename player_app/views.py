98b4896e63f63139df16453f294e2b36f6ec38a3
Oscar Fernandes <oscarthf@gmail.com>
2025-04-25 17:44:54 +0200

    add rate limits

--- player_app/views.py
+++ player_app/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 
+from django_ratelimit.decorators import ratelimit
+
+DEFAULT_RATELIMIT = '100/h'  # Default rate limit for all views
+
+@ratelimit(key='ip', rate=DEFAULT_RATELIMIT)
 def login_view(request):
+
+    if request.user.is_authenticated:
+        return redirect('player')  # or your desired post-login view
+
     return render(request, 'login.html')
 
+@ratelimit(key='ip', rate=DEFAULT_RATELIMIT)
 @login_required
 def home(request):
     return render(request, 'home.html')
@@ -18,10 +28,12 @@ def home(request):
 
 #     return render(request, "home.html")#, {"var": 1})
 
+@ratelimit(key='ip', rate=DEFAULT_RATELIMIT)
 @login_required
 def player(request):
     return render(request, "player.html")#, {"var": 1})
 
+@ratelimit(key='ip', rate=DEFAULT_RATELIMIT)
 @login_required
 def settings(request):
     return render(request, "settings.html")#, {"var": 1})
