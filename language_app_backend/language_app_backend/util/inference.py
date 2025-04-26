df9e1067feac019e501a88ed71077c3fa17653ed
Oscar Fernandes <oscarthf@gmail.com>
2025-04-26 21:29:05 +0200

    dockerfile works

--- language_app_backend/language_app_backend/util/inference.py
+++ language_app_backend/language_app_backend/util/inference.py
@@ -1,11 +1,8 @@
-import os
 
 from openai import OpenAI
 
 def get_inference_client() -> OpenAI:
 
-    cleint = OpenAI(
-        key=os.environ.get("OPENAI_API_KEY")
-    )
+    cleint = OpenAI()
 
     return cleint
