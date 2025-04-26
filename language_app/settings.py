ddfb1601ef88eed08fda9bfe8767d8c012d57495
Oscar Fernandes <oscarthf@gmail.com>
2025-04-26 19:50:11 +0200

    add frontend url

--- language_app/settings.py
+++ language_app/settings.py
@@ -93,6 +93,8 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.environ.get('STRIPE_WEBHOOK_SECRET')
 STRIPE_PRICE_ID = os.environ.get('STRIPE_PRICE_ID')
 
+FRONTEND_URL = os.environ.get('FRONTEND_URL', 'http://localhost:3000')
+
 WSGI_APPLICATION = 'language_app.wsgi.application'
 
 # Database
