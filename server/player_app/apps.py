250cc7c931c7a6f379ec5c1112943ed2bae57a45
Oscar Fernandes <oscarthf@gmail.com>
2025-04-26 22:37:02 +0200

    prevent error on start

--- server/player_app/apps.py
+++ server/player_app/apps.py
@@ -2,12 +2,19 @@ from dotenv import load_dotenv
 
 from django.apps import AppConfig
 
-from language_app_backend.util.db import setup_globals
+is_collecting_static = False
+
+try:
+    from language_app_backend.util.db import setup_globals
+except ImportError:
+    print("ImportError: language_app_backend not found. ")
+    is_collecting_static = True
 
 class PlayerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'player_app'
     
     def ready(self):
-        load_dotenv()
-        setup_globals()
+        if not is_collecting_static:
+            load_dotenv()
+            setup_globals()
