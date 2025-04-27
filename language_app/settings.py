f06e4b041cfc17163970689e33c3f70503823ed3
Oscar Fernandes <oscarthf@gmail.com>
2025-04-27 17:54:49 +0200

    set staticfiles dirs in prod

--- language_app/settings.py
+++ language_app/settings.py
@@ -142,10 +142,9 @@ USE_TZ = True
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [ BASE_DIR / "static" ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
