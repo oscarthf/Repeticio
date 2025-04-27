7e03dec6438adb456aac30eb7f35ccc3e14f2238
Oscar Fernandes <oscarthf@gmail.com>
2025-04-27 17:44:19 +0200

    use RUN_MAIN env var?

--- player_app/apps.py
+++ player_app/apps.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 
 from django.apps import AppConfig
@@ -10,8 +11,9 @@ class PlayerAppConfig(AppConfig):
     
     def ready(self):
         load_dotenv()
-        try:
-            setup_globals()
-        except Exception as e:
-            print(f"Error setting up globals: {e}")
-            raise e
+        if os.environ.get('RUN_MAIN'):
+            try:
+                setup_globals()
+            except Exception as e:
+                print(f"Error setting up globals: {e}")
+                raise e
