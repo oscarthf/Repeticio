3ec047d9611a53c618d539f08fd619c75e89bc66
Oscar Fernandes <oscarthf@gmail.com>
2025-04-27 17:10:43 +0200

    see if backend is installed on start

--- player_app/apps.py
+++ player_app/apps.py
@@ -2,19 +2,12 @@ from dotenv import load_dotenv
 
 from django.apps import AppConfig
 
-is_collecting_static = False
-
-try:
-    from language_app_backend.util.db import setup_globals
-except ImportError:
-    print("ImportError: language_app_backend not found. ")
-    is_collecting_static = True
+from language_app_backend.util.db import setup_globals
 
 class PlayerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'player_app'
     
     def ready(self):
-        if not is_collecting_static:
-            load_dotenv()
-            setup_globals()
+        load_dotenv()
+        setup_globals()
