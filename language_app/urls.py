9784c2aeebcc247d88dbca732f616fb470008924
Oscar Fernandes <oscarthf@gmail.com>
2025-04-29 22:36:11 +0200

    clean up

--- language_app/urls.py
+++ language_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('player_app.urls')),
 ]
 
