3a63607ad0f2627dfb1090d260f9ac8c94737168
Oscar Fernandes <oscarthf@gmail.com>
2025-04-29 22:36:20 +0200

    clean up

--- language_app/urls.py
+++ language_app/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
